dbe5c999441220120dcd3872b0e65f17dd49a7f0
Tsering Lama <84913869+Tsering-Lama@users.noreply.github.com>
2023-08-07 02:43:47 -0400

    Add files via upload

---
+++ day4_Aug5/day4.js
@@ -0,0 +1,19 @@
+function doubleNumber(number){
+    console.log('Classwork day 4')
+    let number=promt("Enter a number")
+    if(number= null){
+        console.log('Enter a number')
+    }
+    else if(isNAN(number)){
+        console.log('Enter a number')
+    }
+    else
+    number *= 2
+    return number
+}
+doubleNumber();
+
+
+//when the function doubleNumber is called and the user didn't enter an argument, number as an argument, the program will ask the user to enter a number. 
+//the function will fully run when the user enters an argument.
+
